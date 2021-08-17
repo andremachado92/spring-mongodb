8501a3d27554ef49b1a673ce460755255e651853
Andre do Rosario Cavalcante Machado <andre.machado@ITBAM.ORG.BR>
2021-08-17 09:36:50 -0400

    [Added] PersonRepository

--- src/main/java/com/andremachado/domain/Person.java
+++ src/main/java/com/andremachado/domain/Person.java
@@ -3,10 +3,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Document(collection = "test")
 public class Person {
 
     @Id
---
+++ src/main/java/com/andremachado/domain/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.andremachado.domain.repository;
+import com.andremachado.domain.Person;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends MongoRepository<Person, String> {
+}
