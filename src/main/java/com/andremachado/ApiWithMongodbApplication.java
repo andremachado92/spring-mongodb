2cfeead3728493dc222677d8101742c2d2a033ea
Andre do Rosario Cavalcante Machado <andre.machado@ITBAM.ORG.BR>
2021-08-17 08:45:37 -0400

    first commit

---
+++ src/main/java/com/andremachado/ApiWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.andremachado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWithMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andremachado/ApiWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremachado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
