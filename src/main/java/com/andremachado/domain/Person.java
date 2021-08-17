4268f78e3076f8fa9e05ae91462ef0f242ce1d0c
Andre do Rosario Cavalcante Machado <andre.machado@ITBAM.ORG.BR>
2021-08-17 09:29:11 -0400

    [Added] data base properties

--- src/main/java/com/andremachado/domain/Person.java
+++ src/main/java/com/andremachado/domain/Person.java
@@ -2,9 +2,16 @@ package com.andremachado.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class Person {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String cellPhone;
 }
